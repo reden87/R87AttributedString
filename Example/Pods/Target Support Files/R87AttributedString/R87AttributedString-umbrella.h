edea479a3d49308c63db730e0415f4467eaca064
Gergo Nemeth <reden87@gmail.com>
2016-02-04 13:37:56 +0100

    enh: initial library added

---
+++ Example/Pods/Target Support Files/R87AttributedString/R87AttributedString-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "NSMutableAttributedString+R87Formatter.h"
+
+FOUNDATION_EXPORT double R87AttributedStringVersionNumber;
+FOUNDATION_EXPORT const unsigned char R87AttributedStringVersionString[];
+
